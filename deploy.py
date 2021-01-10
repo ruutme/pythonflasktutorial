b5e1bdb512fe516f0b576e882d7137d3b44c93fb
ruutme <ruut.autere@gmail.com>
2021-01-10 11:06:43 +0200

    Initial implementation for restful api

--- deploy.py
+++ deploy.py
@@ -6,6 +6,8 @@ from flask import json
 # g stands for "global".
 from flask import g
 
+from flask import request
+
 import sqlite3
 
 DATABASE = 'sample.db'
@@ -26,19 +28,85 @@ def teardown_db(execption):
 def deploy():
     return 'Hello world!'
 
+# Create Read Update Delete (CRUD)
+
+# create one = POST
+@app.route('/item', methods=['POST'])
+def create_item():
+    name = request.form['name']
+    value = request.form['value']
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO items (name, value) VALUES (?,?)', (name, value))
+    conn.commit()
+
+    return app.response_class(
+        response = json.dumps([{'action':'Added new item'}, {'new item': cur.lastrowid}]),
+        status = 200,
+        mimetype = 'application/json'
+    )
+
+# read all = GET
 @app.route('/items')
 def get_items():
     data = []
 
     cur = get_db().cursor()
-    cur.execute('SELECT name, value FROM items')
+    cur.execute('SELECT id, name, value FROM items')
 
     rows = cur.fetchall()
     for row in rows:
-        data.append({'name':row[0], 'value':row[1]})
+        data.append({'id': row[0], 'name': row[1], 'value': row[2]})
 
     return app.response_class(
         response = json.dumps(data),
+        status = 200,
+        mimetype = 'application/json'
+    )
+
+# read one = GET
+@app.route('/item/<int:id>', methods=['GET'])
+def get_item(id):
+    cur = get_db().cursor()
+    cur.execute('SELECT * FROM items WHERE id = ?', (id,))
+
+    item = cur.fetchone()
+    return app.response_class(
+        response = json.dumps([{'action':'testing'}, item]),
+        status = 200,
+        mimetype = 'application/json'
+    )
+
+# update one = PUT
+@app.route('/item', methods=['PUT'])
+def update_item():
+    conn = get_db()
+    cur = conn.cursor()
+
+    id = request.form['id']
+    value = request.form['value']
+
+    cur.execute('UPDATE items SET value = ? WHERE id = ?', (value, id))
+    conn.commit()
+
+    return app.response_class(
+        response = json.dumps([{'action': 'Updated item'}, {'id': id}]),
+        status = 200,
+        mimetype='application/json'
+    )
+
+
+# delete one = DELETE
+@app.route('/item', methods=['DELETE'])
+def delete_item():
+    id = request.form['id']
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute('DELETE FROM items WHERE id = ?',(id,))
+    conn.commit()
+
+    return app.response_class(
+        response = json.dumps([{'action': 'Deleted item'}, {'id':id}]),
         status = 200,
         mimetype = 'application/json'
     )
